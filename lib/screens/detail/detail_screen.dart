509de78a7cef8662eb3510cf96ee7994aece9fe8
AmandaCornelsen <cornelsen089@gmail.com>
2025-08-12 20:55:47 -0300

    Aula 1

---
+++ lib/screens/detail/detail_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:obras_de_arte/models/artwork.dart';
+
+class ArtworkDetailScreen extends StatefulWidget {
+//final Artwork artwork;
+//const ArtworkDetailScreen({super.key, required this.artwork});
+
+  @override
+  State<ArtworkDetailScreen> createState() => _ArtworkDetailScreenState();
+}
+
+class _ArtworkDetailScreenState extends State<ArtworkDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
